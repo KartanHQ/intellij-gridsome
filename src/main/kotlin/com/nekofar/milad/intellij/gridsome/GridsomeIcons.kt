dbf062af95766cdc5e74e2c592d8f334491f80c7
Milad Nekofar <milad@nekofar.com>
2023-07-22 11:20:58 +0330

    refactor: add new Gridsome SVG icons
    Integrating new SVG icons for the Gridsome framework. The diff indicates that three files have changed:
    
    - A new Gridsome SVG icon was added (`gridsome.svg`) along with its representation in Kotlin (`GridsomeIcons.kt`).
    - The existing plugin icon (`pluginIcon.svg`) was updated to match the new Gridsome icon.
    
    The changes are necessary to support the visual representation of Gridsome projects in Intellij IDE. It is part of the ongoing effort to support different frameworks.

---
+++ src/main/kotlin/com/nekofar/milad/intellij/gridsome/GridsomeIcons.kt
@@ -0,0 +1,8 @@
+package com.nekofar.milad.intellij.gridsome
+
+import com.intellij.openapi.util.IconLoader
+
+object GridsomeIcons {
+    @JvmField
+    val ProjectGenerator = IconLoader.getIcon("/icons/gridsome.svg", javaClass)
+}
